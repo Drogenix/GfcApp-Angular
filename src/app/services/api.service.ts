647b09d5475485ba93d433d70bced4c8bcb19067
GundONE <ilyaroshevich@gmail.com>
2022-10-05 23:34:45 +0300

    Изменения работы сервиса.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,72 +5,91 @@ import {Tournament} from "../models/tournament";
 import {Fight} from "../models/fight";
 import { Blog } from '../models/blog';
 import {Fighter} from "../models/fighter";
+import {environment} from "../../environments/environment.prod";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService{
 
+  private _apiUrl = environment.apiUrl;
+
   constructor(private httpClient:HttpClient){}
 
   getFighters():Observable<Fighter[]>
   {
-    return this.httpClient.get<Fighter[]>("https://localhost:7116/api/fighters").pipe(retry(25),catchError(this.handleRequestError))
+    const url = this._apiUrl+'fighters';
+
+    return this.httpClient.get<Fighter[]>(url).pipe(retry(25),catchError(this.handleRequestError))
   }
 
   getFighterById(id:number):Observable<Fighter>
   {
-    let url = 'https://localhost:7116/api/fighters/'+ id;
+    const url = this._apiUrl+'fighters/'+ id;
 
     return this.httpClient.get<Fighter>(url).pipe(retry(25),catchError(this.handleRequestError));
   }
 
   getFightersSortedByRating():Observable<Fighter[]>
   {
-    return this.httpClient.get<Fighter[]>("https://localhost:7116/api/fighters/ratings").pipe(retry(25),catchError(this.handleRequestError))
+    const url = this._apiUrl+'fighters/ratings';
+
+    return this.httpClient.get<Fighter[]>(url).pipe(retry(25),catchError(this.handleRequestError))
   }
 
   getFightersSortedByPfpRating():Observable<Fighter[]>
   {
-    return this.httpClient.get<Fighter[]>("https://localhost:7116/api/fighters/pfpratings").pipe(retry(25),catchError(this.handleRequestError))
+    const url = this._apiUrl+'fighters/pfpratings';
+
+    return this.httpClient.get<Fighter[]>(url).pipe(retry(25),catchError(this.handleRequestError))
   }
 
   getFutureTournaments():Observable<Tournament[]>
   {
-    return this.httpClient.get<Tournament[]>("https://localhost:7116/api/Tournaments/future").pipe(retry(25),catchError(this.handleRequestError))
+    const url = this._apiUrl+'Tournaments/future';
+
+    return this.httpClient.get<Tournament[]>(url).pipe(retry(25),catchError(this.handleRequestError))
   }
 
   getEndedTournaments():Observable<Tournament[]>
   {
-    return this.httpClient.get<Tournament[]>("https://localhost:7116/api/Tournaments/ended").pipe(retry(25),catchError(this.handleRequestError))
+    const url = this._apiUrl+'Tournaments/ended';
+
+
+    return this.httpClient.get<Tournament[]>(url).pipe(retry(25),catchError(this.handleRequestError))
   }
 
   getNearTournament():Observable<Tournament>
   {
-    return this.httpClient.get<Tournament>("https://localhost:7116/api/Tournaments/near").pipe(retry(25),catchError(this.handleRequestError))
+    const url = this._apiUrl+'Tournaments/near';
+
+    return this.httpClient.get<Tournament>(url).pipe(retry(25),catchError(this.handleRequestError))
   }
 
   getTournamentById(id:number):Observable<Tournament>
   {
-    let url = 'https://localhost:7116/api/tournaments/'+ id;
+    const url = this._apiUrl + 'tournaments/'+ id;
 
     return this.httpClient.get<Tournament>(url).pipe(retry(25),catchError(this.handleRequestError))
   }
 
   getFights(fighterId: number):Observable<any>
   {
-    let url = 'https://localhost:7116/api/fights/'+fighterId;
+    const url = this._apiUrl + 'fights/'+fighterId;
+
     return this.httpClient.get<Fight>(url).pipe(retry(25),catchError(this.handleRequestError))
   }
 
   getBlogs():Observable<Blog[]>
   {
-    return this.httpClient.get<Blog[]>("https://localhost:7116/api/blogs").pipe(retry(25),catchError(this.handleRequestError))
+    const url = this._apiUrl + 'blogs';
+
+    return this.httpClient.get<Blog[]>(url).pipe(retry(25),catchError(this.handleRequestError))
   }
 
   getBlogById(id:number):Observable<Blog>
   {
-    let url = 'https://localhost:7116/api/blogs/'+ id;
+    const url = this._apiUrl + 'blogs'+ id;
 
     return this.httpClient.get<Blog>(url).pipe(retry(25),catchError(this.handleRequestError))
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://185.251.38.184/api/'
 };
